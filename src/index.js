ab5ec15053adb1f59af34d5f2d22611cc6cfe0a8
Martin Muriuki <helheim254@gmail.com>
2023-06-19 18:55:44 +0300

    Initialize TaskList class and declare functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  createNewTask(description) {
+    //create new task object
+    const newTask = new Task(description);
+    this.tasks.push(newTask);
+  }
+
+  renderTasks() {
+    //add tasks to object as they appear
+    return this.tasks.map((task) => task.render()).join("");
+  }
+
+  deleteTask(description) {
+    this.tasks = this.tasks.filter((task) => task.description !== description);
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const TaskList = new TaskList();
+  //initialize task class
+  //get user input from form
+  const newForm = document.getElementById("create-task-form");
+  const newTask = document.getElementById("new-task-description");
+
+  //get list of tasks
+  const newTasks = document.getElementById("tasks");
 });
